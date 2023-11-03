64715d20f8ba61eefd48f220e01477d71bab849c
Alexander Gomez Bonilla <alexander.gomez.bonilla@ordina.nl>
2023-11-03 16:01:01 +0100

    added scraping of customer name

--- projects/04-SuperEddie/content.js
+++ projects/04-SuperEddie/content.js
@@ -2,11 +2,24 @@ window.addEventListener("load", (event) => {
   const intervalAssignmentContent = setInterval(() => {
     const koCriteriaContainer = document.querySelector(".kocriteria-container");
     const formblockContainer = document.querySelector(".mx-name-container28 ");
+
+    const clientNameContainer = document.querySelector(".mx-name-text13");
+    const clientLocationContainer = document.querySelector(".mx-name-text31");
+
     const koCriteria = textNodesUnder(koCriteriaContainer);
     const activities = textNodesUnder(formblockContainer);
-    if (koCriteria.length > 1 && activities.length > 1) {
+    const clientName = textNodesUnder(clientNameContainer);
+    const clientLocation = textNodesUnder(clientLocationContainer);
+    if (
+      koCriteria.length > 1 &&
+      activities.length > 1 &&
+      clientName.length > 0 &&
+      clientLocation.length > 0
+    ) {
       console.log("🚀 ~ ASSIGNMENT ~  koCriteria:", koCriteria);
       console.log("🚀 ~ ASSIGNMENT ~  activities:", activities);
+      console.log("🚀 ~ ASSIGNMENT ~  clientName:", clientName);
+      console.log("🚀 ~ ASSIGNMENT ~  clientLocation:", clientLocation);
       clearInterval(intervalAssignmentContent); // Stop the interval when condition is met
     }
   }, 1000);
